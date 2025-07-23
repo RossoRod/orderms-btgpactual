d6d5ba4d00daf335161d23ec6e4e44f5b15c7484
Rodrigo Rosso <rosso_rodrigo@hotmail.com>
2025-07-22 21:58:45 -0300

    valor total dos pedidos do cliente

--- src/main/java/com/rosso/btgpactual/orderms_btgpactual/controller/dto/ApiResponse.java
+++ src/main/java/com/rosso/btgpactual/orderms_btgpactual/controller/dto/ApiResponse.java
@@ -1,7 +1,9 @@
 package com.rosso.btgpactual.orderms_btgpactual.controller.dto;
 
 import java.util.List;
+import java.util.Map;
 
-public record ApiResponse<T>(List<T> data,
+public record ApiResponse<T>(Map<String, Object> summary,
+                             List<T> data,
                              PaginationResponse pagination) {
 }
--- src/main/java/com/rosso/btgpactual/orderms_btgpactual/service/OrderService.java
+++ src/main/java/com/rosso/btgpactual/orderms_btgpactual/service/OrderService.java
@@ -8,19 +8,27 @@ import com.rosso.btgpactual.orderms_btgpactual.listener.dto.OrderItemEvent;
 import com.rosso.btgpactual.orderms_btgpactual.mapper.OrderMapper;
 import com.rosso.btgpactual.orderms_btgpactual.repository.OrderRepository;
 import lombok.AllArgsConstructor;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 
 @Service
 @AllArgsConstructor
 public class OrderService {
     @Autowired
     private final OrderRepository orderRepository;
+    @Autowired
+    private final MongoTemplate mongoTemplate;
     private final OrderMapper orderMapper;
 
     public void save(OrderCreatedEvent event) {
@@ -38,6 +46,20 @@ public class OrderService {
     public Page<OrderResponse> findAllByCustomerId(Long customerId, PageRequest pageRequest) {
         var orders = orderRepository.findAllByCustomerId(customerId, pageRequest);
         return orders.map(orderMapper::fromEntity);
+    }
+
+    public BigDecimal findTotalOnOrdersByCustomerId(Long customerId){
+        var aggregations = newAggregation(
+                match(Criteria.where("customerId").is(customerId)),
+                group().sum("total").as("total")
+        );
+
+        var response = mongoTemplate.aggregate(aggregations, OrderEntity.class, Document.class);
+
+        return Optional.ofNullable(response.getUniqueMappedResult())
+                .map(r -> r.get("total"))
+                .map(val -> new BigDecimal(val.toString()))
+                .orElse(BigDecimal.ZERO);
     }
 
     private BigDecimal getTotal(List<OrderItemEvent> items) {
